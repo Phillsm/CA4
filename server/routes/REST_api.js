7388cd2f8703afd5e76dc269bf6cab9d7fb23c01
Phillsm <github@psmith.dk>
2014-11-19 03:37:04 -0500

    implemented rest service for the four database methods (no written tests)

--- server/routes/REST_api.js
+++ server/routes/REST_api.js
@@ -1,10 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../dataLayer.js')
+router.get('/test',function(req,res){
+  res.end("HI THARE");
+})
 
-var mongoose = require('mongoose');
-var user = mongoose.model('User');
+router.get('/categories',function(req,res){
+  db.getCategories(function(err,data){
+    res.end(JSON.stringify(data));
+  })
+})
 
-/* GET A User From The DataBase */
+router.get('/wikis',function(req,res){
+  if (req.query.title){
+    db.getWiki(req.query.title,function(err,data){
+      res.end(JSON.stringify(data));
+    })
+  }
+  else if (req.query.search){
+    db.findWiki(req.query.search,function(err,data){
+      res.end(JSON.stringify(data));
+    })
+  }
+  else if (req.query.category){
+    db.getWikisWithCategory(req.query.category,function(err,data){
+      res.end(JSON.stringify(data));
+    })
+  }
+  else {
+    res.end("You must call with either a title, category or search  query. for example http://178.62.15.16/wikis?title=Abacus, http://178.62.15.16/wikis?search=turing, http://178.62.15.16/wikis?category=Turing tests")
+  }
+})
+/*
+// GET A User From The DataBase 
 router.get('/user', function(req, res) {
   if(typeof global.mongo_error !== "undefined"){
     res.status(500);
@@ -21,5 +49,5 @@ router.get('/user', function(req, res) {
     res.end(JSON.stringify(users));
   });
 });
-
+*/
 module.exports = router;
