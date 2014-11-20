97023cdce83cac8a8f673678a261abf5d4a55cc8
Phillsm <github@psmith.dk>
2014-11-19 22:11:20 -0500

    wrote tests for the rest service

--- test/backend-tests/restAPITests.js
+++ test/backend-tests/restAPITests.js
@@ -1,4 +1,4 @@
-global.TEST_DATABASE = "mongodb://localhost/TestDataBase_xx1243";
+global.TEST_DATABASE = "mongodb://localhost/wiki"
 
 var should = require("should");
 var app = require("../../server/app");
@@ -6,7 +6,7 @@ var http = require("http");
 var testPort = 9999;
 var testServer;
 var mongoose = require("mongoose");
-var User = mongoose.model("User");
+var bl = require('bl')
 
 describe('REST API for /user', function () {
   //Start the Server before the TESTS
@@ -20,32 +20,51 @@ describe('REST API for /user', function () {
       });
   })
 
-  beforeEach(function(done){
-    User.remove({}, function ()
-    {
-      var array = [{userName : "Lars", email :"lars@a.dk",pw: "xxx"},{userName : "Henrik", email :"henrik@a.dk",pw: "xxx"}];
-      User.create(array,function(err){
-        done();
-      });
-    });
-  })
-
   after(function(){  //Stop server after the test
     //Uncomment the line below to completely remove the database, leaving the mongoose instance as before the tests
     //mongoose.connection.db.dropDatabase();
     testServer.close();
   })
 
-  it("Should get 2 users; Lars and Henrik", function (done) {
-    http.get("http://localhost:"+testPort+"/api/user",function(res){
-      res.setEncoding("utf8");//response data is now a string
-      res.on("data",function(chunk){
-        var n = JSON.parse(chunk);
-        n.length.should.equal(2);
-        n[0].userName.should.equal("Lars");
-        n[1].userName.should.equal("Henrik");
-        done();
-      });
+  it("should find many unique categories",function(done){
+    http.get("http://localhost:"+testPort+"/api/categories",function(res){
+      res.pipe(bl(function(err,data){
+                 var json = JSON.parse(data.toString());
+                 json.length.should.be.above(9000);
+                 done();
+               }))
     })
-  });
+  })
+
+  it("should be able to find wiki by title",function(done){
+    http.get("http://localhost:"+testPort+'/api/wikis?title=Abacus',function(res){
+      res.pipe(bl(function(err,data){
+                 var json = JSON.parse(data.toString());
+                 json[0].title.should.be.type('string');
+                 done();
+               }))
+    })
+  })
+
+  it("should be able to find wikis grouped by category",function(done){
+    http.get("http://localhost:"+testPort+'/api/wikis?category=Silent films',function(res){
+      res.pipe(bl(function(err,data){
+                 var json = JSON.parse(data.toString());
+                 json.length.should.be.above(2);
+                 done();
+               }))
+    })
+  })
+
+  it("should be able to search for wikis",function(done){
+    http.get("http://localhost:"+testPort+'/api/wikis?search=turing',function(res){
+      res.pipe(bl(function(err,data){
+                 var json = JSON.parse(data.toString());
+                 json.length.should.be.above(2);
+                 done();
+               }))
+    })
+  })
+
+
 });
